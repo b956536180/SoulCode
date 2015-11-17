1ff85c8ee44c86de0aa8e3dff415f0cf2c9039f1
b956536180 <956536180@qq.com>
2015-11-17 15:21:17 +0800

    Add BitCode Support

---
+++ SoulCode.framework/Versions/A/Headers/SCMacro.h
@@ -0,0 +1,13 @@
+//
+//  Macro.h
+//  FindAndLove
+//
+//  Created by Xiao on 15/4/7.
+//  Copyright (c) 2015年 Liu_zhifang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define KSCREENWITHED [[UIScreen mainScreen] bounds].size.width
+#define KSCREENHEIGHT [[UIScreen mainScreen] bounds].size.height
+#define kSCREENBOUNDS [UIScreen mainScreen].bounds
